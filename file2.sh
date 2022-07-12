8905aeabb335c68f8e9cc2b99ee5393d1912af76
Bhaskar420 <60222004+Bhaskar420@users.noreply.github.com>
2022-07-12 22:51:19 +0530

    Update file2.sh

--- file2.sh
+++ file2.sh
@@ -1,7 +1,8 @@
 think this best may i know company name
 i expecting monthly sal but your 20 day 
 how handle them
-
+charecter this is called programing table and methode that is 
+called treeple methode charactezation
 
 my suitable also not good cloud give seggestion.
 how to oeoe this ur pandimic
