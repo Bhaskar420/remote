4604e9bfb245c4a8441e6a55adf65f010badae52
bhaskar <bhaskarvalluri123@gmail.com>
2022-08-21 09:20:20 +0530

    this ur main

--- file1.sh
+++ file1.sh
@@ -1,8 +1,18 @@
 <<<<<<< HEAD
 this babi adda 
  no one ur methode that is called 
+<<<<<<< HEAD
 =======
 this ur own methode characterzattion methode that is called programing table ur 
 
 characterazation this ur methode controller
 >>>>>>> rrr
+=======
+this called 
+
+caller ramya superstar 
+kalavathi rebalstar
+charani megastar
+
+>>>>>>> 436f75c37bda0bab766bf80ce6a1f20c9c217b22
+i was born last 1993 repoting calalution this ur methode
